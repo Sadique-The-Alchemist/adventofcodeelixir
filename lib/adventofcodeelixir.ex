5e19be7205c2c6de30a9b96766af923890d54fb0
Mohammed Sadique <msadique@boweryfarming.com>
2024-12-03 15:52:30 +0530

    day3

--- lib/adventofcodeelixir.ex
+++ lib/adventofcodeelixir.ex
@@ -3,16 +3,42 @@ defmodule Adventofcodeelixir do
   Documentation for `Adventofcodeelixir`.
   """
 
-  @doc """
-  Hello world.
+  @regex_for_mul ~r/mul\((\d+),(\d+)\)/
+def day3part1(input) do
+  input
+  |>parse_mul_lines()
+  |>calculate_product()
+end
+defp parse_mul_lines(input) do
+  Regex.scan(@regex_for_mul, input)
+end
 
-  ## Examples
+def day3part2(input) do
+  input
+  |> parse_enabled_mul_lines()
+  |> parse_mul_lines()
+  |>calculate_product()
 
-      iex> Adventofcodeelixir.hello()
-      :world
+end
+defp parse_enabled_mul_lines(input) do
 
-  """
-  def hello do
-    :world
+  partialy_enabled = String.split(input, ["don't"])
+  already_enabled = hd(partialy_enabled)
+  partialy_enabled
+  |>tl()
+  |>Enum.map(fn doable ->
+    doable
+    |>String.split("do")
+    |>tl()
+  end)
+  |>List.insert_at(0, already_enabled)
+  |>List.to_string()
+end
+defp calculate_product(mul_lines) do
+  mul_lines
+  |>Enum.map(fn [_mul, left, right] ->
+   String.to_integer(left) * String.to_integer(right)
+  end)
+  |>Enum.sum()
   end
 end
