ea80b1c0b184d8a74a27e60e6fa6eba62d048b3b
Mohammed Sadique <msadique@boweryfarming.com>
2024-12-03 12:18:53 +0530

    intial

---
+++ lib/adventofcodeelixir.ex
@@ -0,0 +1,18 @@
+defmodule Adventofcodeelixir do
+  @moduledoc """
+  Documentation for `Adventofcodeelixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Adventofcodeelixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/adventofcodeelixir_test.exs
@@ -0,0 +1,8 @@
+defmodule AdventofcodeelixirTest do
+  use ExUnit.Case
+  doctest Adventofcodeelixir
+
+  test "greets the world" do
+    assert Adventofcodeelixir.hello() == :world
+  end
+end
